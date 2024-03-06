45d8fe04de80466f83655629f84430dd363916a4
Hakkush-07 <hakan.karakus@boun.edu.tr>
2024-03-06 18:08:48 +0300

    reorganize main for cutoff

--- groth16-verifier/main.c
+++ groth16-verifier/main.c
@@ -4,41 +4,7 @@
 #include "constants.h"
 
 BYTE buf[SHA256_BLOCK_SIZE];
-
-void complete_public_inputs(unsigned char public_inputs[][32]) {
-    SHA256_CTX output_ctx, claim_ctx, journal_ctx;
-    sha256_init(&journal_ctx);
-    sha256_init(&output_ctx);
-    sha256_init(&claim_ctx);
-    
-    sha256_update(&claim_ctx, RECEIPT_CLAIM_TAG, sizeof(RECEIPT_CLAIM_TAG)/sizeof(unsigned char));
-    sha256_update(&claim_ctx, CLAIM_INPUT, sizeof(CLAIM_INPUT)/sizeof(unsigned char));
-    sha256_update(&claim_ctx, CLAIM_PRE, sizeof(CLAIM_PRE)/sizeof(unsigned char));
-
-    sha256_update(&output_ctx, OUTPUT_TAG, sizeof(OUTPUT_TAG)/sizeof(unsigned char));
-
-    sha256_update(&claim_ctx, CLAIM_POST, sizeof(CLAIM_POST)/sizeof(unsigned char));
-
-    /// CUTOFF
-    sha256_update(&journal_ctx, JOURNAL, sizeof(JOURNAL)/sizeof(unsigned char));
-    sha256_final(&journal_ctx, buf);
-    
-    sha256_update(&output_ctx, buf, SHA256_BLOCK_SIZE);
-    sha256_update(&output_ctx, ZEROS, sizeof(ZEROS)/sizeof(unsigned char));
-    sha256_update(&output_ctx, TWO_U16, sizeof(TWO_U16)/sizeof(unsigned char));
-    sha256_final(&output_ctx, buf);
-
-    sha256_update(&claim_ctx, buf, SHA256_BLOCK_SIZE);
-    sha256_update(&claim_ctx, ZERO_U32, sizeof(ZERO_U32)/sizeof(unsigned char));
-    sha256_update(&claim_ctx, ZERO_U32, sizeof(ZERO_U32)/sizeof(unsigned char));
-    sha256_update(&claim_ctx, FOUR_U16, sizeof(FOUR_U16)/sizeof(unsigned char));
-    sha256_final(&claim_ctx, buf);
-
-    for(int i = 0; i < 16; i++){
-        public_inputs[2][i] = buf[i];
-        public_inputs[3][i] = buf[i + 16];
-    }
-}
+SHA256_CTX output_ctx, claim_ctx, journal_ctx;
 
 typedef struct {
     mclBnG1 a;
@@ -85,9 +51,8 @@ VerifyKey get_vk() {
 }
 
 PublicInputs get_public_inputs() {
-    complete_public_inputs(BYTES_PUBLIC_INPUTS);
     PublicInputs public_inputs;
-    for (int i = 0; i < NPI; i++) {
+    for (int i = 0; i < NPI / 2; i++) {
         mclBnFr_setLittleEndian(&public_inputs.public[i], BYTES_PUBLIC_INPUTS[i], 32);
     }
     return public_inputs;
@@ -123,8 +88,45 @@ int verify_proof(VerifyKey vk, Proof proof, PublicInputs public_inputs) {
 int main() {
     int ret = mclBn_init(MCL_BN_SNARK1, MCLBN_COMPILED_TIME_VAR);
     if (ret != 0) {return 1;}
+
     VerifyKey vk = get_vk();
+
+    sha256_init(&journal_ctx);
+    sha256_init(&output_ctx);
+    sha256_init(&claim_ctx);
+    sha256_update(&claim_ctx, RECEIPT_CLAIM_TAG, sizeof(RECEIPT_CLAIM_TAG)/sizeof(unsigned char));
+    sha256_update(&claim_ctx, CLAIM_INPUT, sizeof(CLAIM_INPUT)/sizeof(unsigned char));
+    sha256_update(&claim_ctx, CLAIM_PRE, sizeof(CLAIM_PRE)/sizeof(unsigned char));
+    sha256_update(&output_ctx, OUTPUT_TAG, sizeof(OUTPUT_TAG)/sizeof(unsigned char));
+    sha256_update(&claim_ctx, CLAIM_POST, sizeof(CLAIM_POST)/sizeof(unsigned char));
+
+    // not fully initialized (missing 3rd and 4th public inputs)
     PublicInputs public_inputs = get_public_inputs();
+
+    /// CUTOFF
+
+    sha256_update(&journal_ctx, JOURNAL, sizeof(JOURNAL)/sizeof(unsigned char));
+    sha256_final(&journal_ctx, buf);
+    sha256_update(&output_ctx, buf, SHA256_BLOCK_SIZE);
+    sha256_update(&output_ctx, ZEROS, sizeof(ZEROS)/sizeof(unsigned char));
+    sha256_update(&output_ctx, TWO_U16, sizeof(TWO_U16)/sizeof(unsigned char));
+    sha256_final(&output_ctx, buf);
+    sha256_update(&claim_ctx, buf, SHA256_BLOCK_SIZE);
+    sha256_update(&claim_ctx, ZERO_U32, sizeof(ZERO_U32)/sizeof(unsigned char));
+    sha256_update(&claim_ctx, ZERO_U32, sizeof(ZERO_U32)/sizeof(unsigned char));
+    sha256_update(&claim_ctx, FOUR_U16, sizeof(FOUR_U16)/sizeof(unsigned char));
+    sha256_final(&claim_ctx, buf);
+
+    for(int i = 0; i < 16; i++){
+        BYTES_PUBLIC_INPUTS[2][i] = buf[i];
+        BYTES_PUBLIC_INPUTS[3][i] = buf[i + 16];
+    }
+    
+    // fill the missing public inputs
+    for (int i = NPI / 2; i < NPI; i++) {
+        mclBnFr_setLittleEndian(&public_inputs.public[i], BYTES_PUBLIC_INPUTS[i], 32);
+    }
+
     Proof proof = get_proof();
     int a = verify_proof(vk, proof, public_inputs);
     return 1 - a;
