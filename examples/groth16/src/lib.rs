44c0991df9ac2c6fed270f27bb43dfd2d45eeef9
Hakkush-07 <hakan.karakus@std.bogazici.edu.tr>
2024-03-20 14:40:09 +0300

    risc0 groth16 for simple proof

--- examples/groth16/src/lib.rs
+++ examples/groth16/src/lib.rs
@@ -2,10 +2,27 @@
 
 pub use groth16_methods::VERIFY_GROTH16_ELF;
 use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
+use risc0_groth16::{ProofJson, PublicInputsJson, Verifier, VerifyingKeyJson};
+
+const VK: &str = include_str!("../data/vk.json");
+const PROOF: &str = include_str!("../data/proof.json");
+const PUBLIC: &str = include_str!("../data/public.json");
 
 pub fn verify_groth16() -> (Receipt, u32) {
     let mut env = ExecutorEnv::builder();
+
     let a: u32 = 5;
+
+    println!("groth16 verify start");
+    let verifying_key: VerifyingKeyJson = serde_json::from_str(VK).unwrap();
+    let proof: ProofJson = serde_json::from_str(PROOF).unwrap();
+    let public_inputs = PublicInputsJson {
+        values: serde_json::from_str(PUBLIC).unwrap(),
+    };
+    let verifier = Verifier::from_json(proof, public_inputs, verifying_key).unwrap();
+    verifier.verify().unwrap();
+    println!("groth16 verify done");
+
     env.write(&a).unwrap();
     let env = env.build().unwrap();
 
